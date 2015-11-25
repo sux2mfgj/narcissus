8491496cf37beb783a775ad964fc54ac9eed4dc9
himaaaatti <sux2mfgj@gmail.com>
2015-11-25 10:40:04 +0900

    added boost program_options

--- src/main.cpp
+++ src/main.cpp
@@ -2,23 +2,50 @@
 #include <fstream>
 #include <memory>
 
+#include <boost/program_options.hpp>
+
 #include <cpu.hpp>
 #include <debug.hpp>
 
 int main(int argc, char const* argv[])
 {
     using namespace narcissus;
-    using namespace std;
+    namespace po = boost::program_options;
 
-    fstream file;
+    try {
+            
+        // program_options
+        po::options_description desc("Allowd options");
+        desc.add_options()
+            ("help", "print this menu")
+        ;
+
+        po::variables_map vm;
+        po::store(po::parse_command_line(argc, argv, desc), vm);
+        po::notify(vm);
+
+        if(vm.count("help"))
+        {
+            std::cout << desc << std::endl;
+            return 0;
+        }
+    } catch (std::exception& e) 
+    {
+        std::cerr << "error: \n\t"
+            << e.what() << std::endl;
+        return 1;
+    }
+
+
+    std::fstream file;
     char buf[16];
 
-    array<uint8_t, (std::uint32_t)h8_3069f::mem_info::rom_size> mem = {0};
+    std::array<uint8_t, (std::uint32_t)h8_3069f::mem_info::rom_size> mem = {0};
 
     if(argc < 2){
         return -1;
     }
-    file.open(argv[1], ios::in | ios::binary);
+    file.open(argv[1], std::ios::in | std::ios::binary);
 
     auto i = 0;
     while (!file.eof()) {
